faa738d22d35d20b0955212d081c1d5cd8ae58ff
atozzz <yuyuyu0926@icloud.com>
2017-10-28 18:29:02 +0900

    Automate assigning date

--- change_log_memo.rb
+++ change_log_memo.rb
@@ -1,15 +1,38 @@
 #!/usr/bin/env ruby
 
-def write_to_changelog(memo, date)
-  write_date(date)
-  write_memo(memo)
+def write_to_changelog(memo)
+  write_date
+  write_memo memo
 end
 
-def write_date(date)
-  return if date.nil?
+def write_date
+  return if check_if_wrote_today
 
+  today = Time.now.strftime("%Y-%m-%d").to_s
   File.open(ENV["HOME"] + "/ChangeLog", "a") do |file|
-    file.write "\n" + date + "\n"
+    file.write "\n" + today + " Foo bar <foobar@hoge.com>" + "\n"
+  end
+
+  update_wrote_log
+end
+
+def check_if_wrote_today
+  today = Time.now.strftime("%Y-%m-%d").to_s
+
+  File.open("./pre_wrote_time_log.txt") do |file|
+    pre_date = file.read.strip
+    if pre_date.empty? # first time
+      return false
+    end
+
+    (today == pre_date) ? true : false;
+  end
+end
+
+def update_wrote_log
+  today = Time.now.strftime("%Y-%m-%d").to_s
+  File.open("./pre_wrote_time_log.txt", "w+") do |file|
+    file.write today
   end
 end
 
@@ -21,4 +44,5 @@ def write_memo(memo)
   end
 end
 
-write_to_changelog(ARGV[0], ARGV[1])
+
+write_to_changelog(ARGV[0])
